e4921458f2fac729f52cb332703d6653b08fa5c7
eleallegue <elenalleglez@gmail.com>
2018-05-07 21:32:10 +0200

    loader changes in order to work

--- src/main/java/es/uniovi/asw/loader_client/Application.java
+++ src/main/java/es/uniovi/asw/loader_client/Application.java
@@ -1,5 +1,6 @@
 package es.uniovi.asw.loader_client;
 
+import es.uniovi.asw.loader_client.database.InsertAgents;
 import es.uniovi.asw.loader_client.parser.ExcelParser;
 
 public class Application {
